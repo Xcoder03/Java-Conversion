7bec57d6eeae9d352289ceabe8a6ccb12e70c7df
Xcoder <chindaebube03@gmail.com>
2023-03-15 19:24:38 +0100

    created a method to convert to celsius

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,7 +1,42 @@
+import java.util.Scanner;
+
 public class TempConversion {
-     public void menu(){
+     public  static void menu(){
+
+         Scanner a = new Scanner(System.in);
+         Scanner value = new Scanner(System.in);
          System.out.println("Welcome to Temperature Conversion");
          System.out.println("Getting started......");
-         System.out.println("Please which temperature would you like to convert to");
+         int option;
+         double temp;
+
+
+         do {
+             System.out.println("Please which temperature would you like to convert to");
+             System.out.println(">>>> Enter an option  <<<< \n >> (1) Convert To Celsius\n >> (2) Convert To Fahrenheit\n >> (3) Back To Menu");
+              option = a.nextInt();
+              switch (option){
+                  case 1:
+                      System.out.println("Enter a value");
+                      temp = value.nextDouble();
+                      break;
+                  case 2:
+                      System.out.println("Enter a value");
+                       temp = value.nextDouble();
+                      break;
+
+                  case 3:
+                      System.out.println("Closing......");
+                      break;
+                  default:
+                      System.out.println("  >>  Enter valid option");
+
+              }
+         }while (option != 3);
+     }
+
+
+     public static   double convertToCelsius (double value){
+            double ans = (value )
      }
 }
