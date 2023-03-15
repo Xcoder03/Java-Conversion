4cd17697857ca461e16d892f97838c858ad08826
Xcoder <chindaebube03@gmail.com>
2023-03-15 18:59:42 +0100

    added a menu method to the temperature conversion class

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class TempConversion {
+public class TempConversion {
+     public void menu(){
+         System.out.println("Welcome to Temperature Conversion");
+         System.out.println("Getting started......");
+         System.out.println("Please which temperature would you like to convert to");
+     }
 }
