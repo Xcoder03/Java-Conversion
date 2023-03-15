ea611a0e23a32cb1ac66ea9ede9567a0982cb21d
Xcoder <chindaebube03@gmail.com>
2023-03-15 19:28:23 +0100

    added a method to convert to fahrenheit in the temperature class

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -37,6 +37,14 @@ public class TempConversion {
 
 
      public static   double convertToCelsius (double value){
-            double ans = (value )
+            double ans = (value - 32 )  * 5/9;
+            return  ans;
      }
+
+    public static   double convertToFahrenheit (double value){
+        double ans = (value *  9/5 ) + 32;
+        return  ans;
+    }
+
+
 }
